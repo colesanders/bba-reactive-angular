9ec065c6dad0fe9eb7e28fd49249f80d0e9c06a9
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-07-26 15:43:53 -0700

    update: cleaing up actions

--- libs/core-state/src/lib/courses/courses.actions.ts
+++ libs/core-state/src/lib/courses/courses.actions.ts
@@ -1,24 +1,2 @@
 import { Course } from '@bba/api-interfaces';
 import { createAction, props } from '@ngrx/store';
-
-// HELPER: Initial state
-const inititalState = [
-  {
-    id: '1',
-    title: 'Angular Course',
-    description: 'This is an Angular course',
-  },
-  {
-    id: '2',
-    title: 'React Course',
-    description: 'This is a React course',
-  },
-];
-
-// HELPER: Immutable operations
-const create = (collection, obj) => [...collection, obj];
-const update = (collection, obj) =>
-  collection.map((i) => {
-    return i.id === obj.id ? Object.assign({}, obj) : i;
-  });
-const remove = (collection, obj) => collection.filter((i) => i.id === obj.id);
