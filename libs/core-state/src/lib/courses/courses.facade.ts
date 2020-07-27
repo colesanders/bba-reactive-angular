8c2cecff556308afb125b108f013f650c1b439b7
Josh de Gouveia <josh.degouveia@hach.com>
2020-07-27 09:59:00 -0700

    courses feature

--- libs/core-state/src/lib/courses/courses.facade.ts
+++ libs/core-state/src/lib/courses/courses.facade.ts
@@ -1,59 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Course } from '@bba/api-interfaces';
-import { CoursesService } from '@bba/core-data';
 import { Action, ActionsSubject, select, Store } from '@ngrx/store';
-import { Subject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-
-import * as fromCourses from './courses.reducer';
+import { filter } from 'rxjs/operators';
 
 import * as CoursesActions from './courses.actions';
+import * as fromCourses from './courses.reducer';
 import * as CoursesSelectors from './courses.selectors';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesFacade {
-  private allCourses = new Subject<Course[]>();
-  private selectedCourse = new Subject<Course>();
-  private mutations = new Subject();
+  selectedCourse$ = this.store.pipe(select(CoursesSelectors.getSelectedCourse));
+  allCourses$ = this.store.pipe(select(CoursesSelectors.getAllCourses));
 
-  selectedCourses$ = this.selectedCourse.asObservable();
-  mutations$ = this.mutations.asObservable();
-
-  allCourses$ = this.store.pipe(
-    select('courses'),
-    map(state => state.courses)
+  mutations$ = this.actions$.pipe(
+    filter((action: Action) =>
+      action.type === CoursesActions.createCourse({} as any).type ||
+      action.type === CoursesActions.updateCourse({} as any).type ||
+      action.type === CoursesActions.deleteCourse({} as any).type
+    )
   );
 
   constructor(
-    private coursesService: CoursesService,
-    private store: Store<fromCourses.CoursesPartialState>
+    private store: Store<fromCourses.CoursesPartialState>,
+    private actions$: ActionsSubject
   ) {}
 
-  reset() {
-    this.mutations.next(true);
-  }
-
   selectCourse(course: Course) {
-    this.selectedCourse.next(course);
+    this.dispatch(CoursesActions.selectCourse({ selectedId: course?.id }));
   }
 
   loadCourses() {
-    this.coursesService
-      .all()
-      .subscribe((courses: Course[]) => this.allCourses.next(courses));
+    this.dispatch(CoursesActions.loadCourses())
   }
 
   createCourse(course: Course) {
-    this.coursesService.create(course).subscribe((_) => this.reset());
+    this.dispatch(CoursesActions.createCourse({ course }));
   }
 
   updateCourse(course: Course) {
-    this.coursesService.update(course).subscribe((_) => this.reset());
+    this.dispatch(CoursesActions.updateCourse({ course }));
   }
 
   deleteCourse(course: Course) {
-    this.coursesService.delete(course.id).subscribe((_) => this.reset());
+    this.dispatch(CoursesActions.deleteCourse({ course }));
+  }
+
+  dispatch(action: Action) {
+    this.store.dispatch(action);
   }
 }
