c45d1242af251d9faac6683ba84a5f98d6d283b8
Josh de Gouveia <josh.degouveia@hach.com>
2020-07-27 09:56:01 -0700

    courses feature

--- libs/core-state/src/lib/courses/courses.facade.ts
+++ libs/core-state/src/lib/courses/courses.facade.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Course } from '@bba/api-interfaces';
-import { CoursesService } from '@bba/core-data';
 import { Action, ActionsSubject, select, Store } from '@ngrx/store';
-import { Subject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-
-import * as fromCourses from './courses.reducer';
+import { filter } from 'rxjs/operators';
 
 import * as CoursesActions from './courses.actions';
+import * as fromCourses from './courses.reducer';
 import * as CoursesSelectors from './courses.selectors';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesFacade {
-  private selectedCourse = new Subject<Course>();
-
-  selectedCourses$ = this.selectedCourse.asObservable();
-  allCourses$ = this.store.pipe(
-    select('courses'),
-    map((state) => state.courses)
-  );
+  selectedCourse$ = this.store.pipe(select(CoursesSelectors.getSelectedCourse));
+  allCourses$ = this.store.pipe(select(CoursesSelectors.getAllCourses));
 
   mutations$ = this.actions$.pipe(
     filter((action: Action) =>
@@ -31,22 +23,16 @@ export class CoursesFacade {
   );
 
   constructor(
-    private coursesService: CoursesService,
     private store: Store<fromCourses.CoursesPartialState>,
     private actions$: ActionsSubject
   ) {}
 
   selectCourse(course: Course) {
-    this.selectedCourse.next(course); // temporary
     this.dispatch(CoursesActions.selectCourse({ selectedId: course?.id }));
   }
 
   loadCourses() {
-    this.coursesService
-      .all()
-      .subscribe((courses: Course[]) =>
-        this.dispatch(CoursesActions.loadCourses({ courses }))
-      );
+    this.dispatch(CoursesActions.loadCourses())
   }
 
   createCourse(course: Course) {
