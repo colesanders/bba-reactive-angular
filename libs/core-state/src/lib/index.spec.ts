651a41d598191f139a7f869237f7dc6e669544a1
Cole Sanders <cole.sanders@briebug.com>
2020-08-16 10:59:08 -0600

    100% coverage on core-state

--- libs/core-state/src/lib/index.spec.ts
+++ libs/core-state/src/lib/index.spec.ts
@@ -1,3 +1,24 @@
+import { mockLesson, mockCourse } from "./tests.mocks";
+import { getCourseLessons } from '.';
+
 describe('Index Common Selector', () => {
-    
+    let lessons, courses;
+
+    beforeEach(() => {
+        lessons = [
+            {...mockLesson, id: '0'},
+            {...mockLesson, id: '1'},
+            {...mockLesson, id: '2'},
+        ]
+        courses = [
+            {...mockCourse, id: '0'},
+            {...mockCourse, id: '1'},
+            {...mockCourse, id: '2'},
+        ]
+    })
+
+
+    it('should select the related courses and lessons', () => {
+        expect(getCourseLessons.projector(courses, lessons)).toMatchSnapshot();
+    })
 })
